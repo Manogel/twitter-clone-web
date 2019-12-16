7cd9fc32f96da435c7bcea3ecc6c9dede2a2b926
Manogel <manoel_ggomes@hotmail.com>
2019-12-16 12:17:37 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import TopBar from './components/TopBar';
+import Profile from './components/Profile';
+import Timeline from './components/Timeline';
+import Widgets from './components/Widgets';
+import { WrapperContent } from './styles'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Banner />
+      <TopBar />
+      <WrapperContent>
+        <Profile />
+        <Timeline />
+        <Widgets />
+      </WrapperContent>
+   </>
   );
 }
 
