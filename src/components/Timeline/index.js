207a6ed459863f19fbfcc02acee99320e878de9c
Manogel <manoel_ggomes@hotmail.com>
2019-12-16 17:38:01 -0300

    timeline ok

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,9 +1,45 @@
-import React from 'react';
-
-import { Container } from './styles';
+import React from "react";
+import { Container, Tweets, Menu } from "./styles";
+import avatar from "../../assets/avatar.png";
+import comments from "../../assets/icons/comments.svg";
+import retweet from "../../assets/icons/retweet.svg";
+import like from "../../assets/icons/like.svg";
 
 export default function Timeline() {
   return (
-    <Container />
+    <Container>
+      <Menu>
+        <a href="google.com" className="active">
+          Tweets
+        </a>
+        <a href="google.com">Tweets and replies</a>
+        <a href="google.com">Medias</a>
+      </Menu>
+      <Tweets>
+        <li>
+          <img src={avatar} alt="Avatar" />
+          <div className="info">
+            <strong>
+              Benoît Vrins <span>@Exibit</span>
+            </strong>
+            <p>
+              sadsadsa djhashasgfas fghafghafgsfagfds ghkfdgjksfgddddd jlgj
+              lfgsa dasdsasdaad dsd sa dasda dadasdsda sdd
+            </p>
+            <div className="actions">
+              <a href="google.com">
+                <img src={comments} alt="Comments" /> 3
+              </a>
+              <a href="google.com">
+                <img src={retweet} alt="Retweet" /> 4
+              </a>
+              <a href="google.com">
+                <img src={like} alt="Like" /> 5
+              </a>
+            </div>
+          </div>
+        </li>
+      </Tweets>
+    </Container>
   );
 }
--- src/components/Timeline/styles.js
+++ src/components/Timeline/styles.js
@@ -1,5 +1,91 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  flex: 1;
+  background: #fff;
+  margin: 10px 20px 0;
+`;
+
+export const Menu = styled.nav`
+  border-bottom: 1px solid #efecf0;
+  padding: 10px 15px;
+
+  a {
+    text-decoration: none;
+    color: #3bb9e3;
+    font-size: 18px;
+    font-weight: bold;
+    margin-left: 20px;
+  }
+
+  a:first-child {
+    margin-left: 0;
+  }
+
+  a.active {
+    color: #1f2226;
+  }
+`;
+
+export const Tweets = styled.ul`
+  list-style: none;
+
+  li {
+    border-bottom: 1px solid #e6ecf0;
+    padding: 10px 15px;
+    display: flex;
+
+    > img {
+      width: 48px;
+      height: 48px;
+      border-radius: 50%;
+    }
+
+    div.info {
+      margin-left: 10px;
+      display: flex;
+      flex-direction: column;
+
+      strong {
+        font-size: 14px;
+        color: #1f2223;
+
+        span {
+          font-weight: normal;
+          font-size: 13px;
+          color: #7b8b9a;
+        }
+      }
+
+      p {
+        font-size: 14px;
+        color: #1f2223;
+        margin-top: 5px;
+        text-align: justify;
+      }
+    }
+
+    div.actions {
+      display: flex;
+      margin-top: 10px;
+
+      a {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        color: #667580;
+        font-weight: bold;
+        font-size: 12px;
+        margin-left: 30px;
+
+        img {
+          margin-right: 5px;
+        }
+      }
+
+      a:first-child {
+        margin-left: 0px;
+      }
+    }
+  }
 `;
