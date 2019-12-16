92b7e5d33412ae453ef52d34abadba92b84f3390
Manogel <manoel_ggomes@hotmail.com>
2019-12-16 12:16:56 -0300

    banner ok

---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Banner() {
+  return (
+    <Container >
+      <h1>Twitter clone aplication</h1>
+    </Container>
+  );
+}
